4cedd70bd0fd58605a8680ec76d4b5fad36260a8
mellowboyxd <chamerallyishfaq@gmail.com>
2026-06-25 19:27:27 -0400

    wip: format

--- src/main.c
+++ src/main.c
@@ -10,59 +10,57 @@ void MainUpdate(GameState *gameState, float dt);
 
 int main(void)
 {
-        LOG(L_INFO, "Hello brave new world!");
-        InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Space Shooter");
-        SetTargetFPS(FPS);
-        ToggleFullscreen();
-        
-        GameState gameState = { 0 };
+	LOG(L_INFO, "Hello brave new world!");
+	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Space Shooter");
+	SetTargetFPS(FPS);
+	ToggleFullscreen();
 
-        InitGameState(&gameState);
+	GameState gameState = { 0 };
 
-        while (!WindowShouldClose()) {
-                float dt = GetFrameTime();
-                MainDraw(&gameState);
-                MainUpdate(&gameState, dt);
-        }
+	InitGameState(&gameState);
 
-        CloseWindow();
-        return 0;
+	while (!WindowShouldClose()) {
+		float dt = GetFrameTime();
+		MainDraw(&gameState);
+		MainUpdate(&gameState, dt);
+	}
+
+	CloseWindow();
+	return 0;
 }
 
 void InitGameState(GameState *gameState)
 {
-        gameState->currentState = STATE_PLAY;
-        gameState->uiPanel = (Rectangle) {
-                .x = (1 - UI_PANEL_RATIO) * GetScreenWidth(), 
-                .y = 0, 
-                .width = UI_PANEL_RATIO * GetScreenWidth(), 
-                .height = GetScreenHeight()
-        };
-        InitPlayer(&gameState->player);
-        InitBullets(gameState->bulletPool);
-        InitEnemies(gameState->enemyPool);
+	gameState->currentState = STATE_PLAY;
+	gameState->uiPanel =
+		(Rectangle){ .x = (1 - UI_PANEL_RATIO) * GetScreenWidth(),
+			     .y = 0,
+			     .width = UI_PANEL_RATIO * GetScreenWidth(),
+			     .height = GetScreenHeight() };
+	InitPlayer(&gameState->player);
+	InitBullets(gameState->bulletPool);
+	InitEnemies(gameState->enemyPool);
 }
 
-void MainDraw(GameState *gameState) 
+void MainDraw(GameState *gameState)
 {
-        BeginDrawing();
-        DrawRectangleRec(gameState->uiPanel, BLACK);
-        DrawRectangle(gameState->uiPanel.x, gameState->uiPanel.y, 2, 
-                        gameState->uiPanel.height, RAYWHITE);
+	BeginDrawing();
+	DrawRectangleRec(gameState->uiPanel, BLACK);
+	DrawRectangle(gameState->uiPanel.x, gameState->uiPanel.y, 2,
+		      gameState->uiPanel.height, RAYWHITE);
 
-        ClearBackground(BLACK);
-        DrawFPS(0, 0);
-        DrawBullets(gameState->bulletPool);
-        DrawPlayer(gameState->player);
-        DrawEnemies(gameState->enemyPool);
+	ClearBackground(BLACK);
+	DrawFPS(0, 0);
+	DrawBullets(gameState->bulletPool);
+	DrawPlayer(gameState->player);
+	DrawEnemies(gameState->enemyPool);
 
-        EndDrawing();
+	EndDrawing();
 }
 
 void MainUpdate(GameState *gameState, float dt)
 {
-        UpdatePlayer(gameState, dt);
-        UpdateBullets(gameState, dt);
-        UpdateEnemies(gameState, dt);
+	UpdatePlayer(gameState, dt);
+	UpdateBullets(gameState, dt);
+	UpdateEnemies(gameState, dt);
 }
-
