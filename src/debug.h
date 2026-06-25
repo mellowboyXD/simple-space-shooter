b176ce8e64ff61feb7bd1480ac5b6aeebb64e894
mellowboyxd <chamerallyishfaq@gmail.com>
2026-06-24 21:56:36 -0400

    wip: fix bug in LOG

--- src/debug.h
+++ src/debug.h
@@ -3,15 +3,17 @@
 
 typedef enum {L_INFO, L_WARN, L_ERROR} LogLevel;
 
-#define LOG(logLevel, fmt, ...) __log(logLevel, __FILE__, __LINE__, \
-                fmt __VA_OPT__(,) __VA_ARGS__)
-
 #ifdef DEBUG
 
 void __log(LogLevel logLevel, const char *file, int line, const char *fmt, ...);
 
+#define LOG(logLevel, fmt, ...) __log(logLevel, __FILE__, __LINE__, \
+                fmt __VA_OPT__(,) __VA_ARGS__)
+
 #else
-        #define __log(...) ((void) 0)
+
+#define LOG(logLevel, ...) ((void) 0)
+
 #endif // DEBUG
 
 #endif // DEBUG_H
